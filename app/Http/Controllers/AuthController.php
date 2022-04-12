f657b75da677522d078a028f1a9aba708c1a0726
Devprecious <oladelep77@gmail.com>
2022-04-12 05:48:10 -0400

    Sanctum Authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users',
+            'phone_number' => 'required|string|max:255|unique:users',
+            'address' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'phone_number' => $request->phone_number,
+            'address' => $request->address,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('nidfultoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('username', $request->username)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message' => 'Incorrect username or password'
+            ], 401);
+        }
+
+        $token = $user->createToken('nidfultoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth('sanctum')->user()->tokens()->delete();
+
+        return [
+            'message' => 'Successfully logged out',
+
+        ];
+    }
+}
